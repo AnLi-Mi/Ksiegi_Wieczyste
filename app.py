c4ed8ebd17fd0cb6ddcb3cc8505f60413f330dea
AnLi-Mi <a.lipiarz@gmail.com>
2020-11-02 14:02:02 +0100

    extracting the last element of the id of ksiega wieczysta

--- app.py
+++ app.py
@@ -19,3 +19,22 @@ def creating_decoder():
     decoding_dict['/']=0
 
     return decoding_dict
+
+ksiega_wieczysta = 'kr/1468/anna'
+numerical = []
+
+for element in ksiega_wieczysta:
+    numerical.append(creating_decoder()[element])
+
+total = []
+
+for num in numerical[0::3]:
+    total.append(num*1)
+
+for num in numerical[1::3]:
+    total.append(num*3)
+
+for num in numerical[2::3]:
+    total.append(num*7)
+
+print (sum(total)%10)
