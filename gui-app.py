1325f8c079e3793d0a8c9f19e0a12435bab12561
AnLi-Mi <a.lipiarz@gmail.com>
2020-11-03 17:58:03 +0100

    implementing the core code with app's functions

--- gui-app.py
+++ gui-app.py
@@ -1,5 +1,63 @@
 from tkinter import *
 
+def encoding_key_for_kw():
+
+    #preparing lists of elemets before and after encoding
+    #in order to turn them into a dictionary by zipping later
+    entered_element=[]
+    returned_element=[]
+
+    # each letter's result after encoding is it's position in aphabet +10
+    # (staring from 1, not 0)
+    for letter in 'abcdefghijklmnoprstuwxyz':
+        entered_element.append(letter)
+        returned_element.append('abcdefghijklmnoprstuwxyz'.index(letter)+11)
+
+    # zipping both lists to create a dictionary of a letter as a key
+    # and it's numerical representation after encoding as a value
+    decoding_dict=dict(zip(entered_element,returned_element))
+
+    # adding the digits (as string) and their numerical representation
+    # after encoding (the same digit) to the distionary
+    for num in range (0,10):
+        decoding_dict[str(num)]=num
+
+    #returning complete dictionary
+    return decoding_dict
+
+# generating a last number of the Księga Wieczysta based on inputed base string
+def decoding(numer_ksiegi, decoder):
+    # editing users input so it's we can perform further operations on it
+    numer_ksiegi=numer_ksiegi.replace(" ", "")
+    numer_ksiegi=numer_ksiegi.replace("/", "")
+    numer_ksiegi=numer_ksiegi.lower()
+
+    # encoding the entered string using the encoding_key_for_kw()
+    decoded_elements = []
+    for element in numer_ksiegi:
+        decoded_elements.append(decoder()[element])
+
+    #preparing a list for decoded_elements after specific muliplications
+    total = []
+
+    # every third element starting from the first one is multipled by 1
+    for num in decoded_elements[0::3]:
+        total.append(num*1)
+
+    # every third element starting from the second one is multipled by 3
+    for num in decoded_elements[1::3]:
+        total.append(num*3)
+
+    # every third element starting from the third one is multipled by 7
+    for num in decoded_elements[2::3]:
+        total.append(num*7)
+
+    # return the rest from dividing by 10 the sum of all numbers
+    # after specific multiplications
+    return f'Cyfra kontrolna księgi {numer_ksiegi} to {sum(total)%10}'
+
+#------- Creating the GUI ----------------
+
 window = Tk()
 window.title("Księgi Wieczyste")
 
