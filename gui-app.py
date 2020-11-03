a1cc79cf61d1e94823c656e524135429c3c7e52a
AnLi-Mi <a.lipiarz@gmail.com>
2020-11-03 17:47:14 +0100

    added label, enter and button elements

--- gui-app.py
+++ gui-app.py
@@ -3,4 +3,14 @@ from tkinter import *
 window = Tk()
 window.title("Księgi Wieczyste")
 
+input_command = Label (window, text = "Wpisz bazę numeru Księgi Wieczystej: ")
+input_command.pack()
+
+user_input = Entry (window)
+user_input.pack()
+user_input.get()
+
+submit_button = Button (window, text = "Sprawdź numer kontrolny")
+submit_button.pack()
+
 window.mainloop()
